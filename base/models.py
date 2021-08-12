4a89cceb204349d3714ab1cb684bc20abfd3e0ae
nearFuture00 <enes.ali.karakobak@gmail.com>
2021-08-12 13:17:58 +0300

    Add "liked_tracks" field to "Artist" model for liking mechanism

--- base/models.py
+++ base/models.py
@@ -23,6 +23,7 @@ class Artist(models.Model):
     banner = models.ImageField(upload_to=bannerPath)
     description = models.TextField(max_length=256)
     following = models.ManyToManyField("self", related_name='followers', blank=True)
+    liked_tracks = models.ManyToManyField("base.Track", related_name="likes", blank=True)
 
     def __str__(self):
         return self.nickname
